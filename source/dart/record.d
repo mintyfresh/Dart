286b624a578ff2461484304947fb09b7da23c9d4
Mihail-K <Mihail.K@fimu.tk>
2014-12-21 13:50:22 -0500

    Added column annotation types.

--- source/dart/record.d
+++ source/dart/record.d
@@ -4,6 +4,7 @@ module dart.record;
 class ColumnInfo {
 
     string name;
+    string field;
 
     bool notNull;
     bool autoIncrement;
@@ -14,8 +15,11 @@ class ColumnInfo {
 
 class Record {
 
-    protected {
+    protected static {
 
+        /**
+         * The column info table, for this record type.
+         **/
         ColumnInfo[string] _columns;
 
         /**
@@ -36,19 +40,76 @@ class Record {
 
 }
 
-mixin template ActiveRecord() {
+alias Target(alias T) = T;
+
+struct Column {
+    string name;
+}
+
+enum Nullable;
+enum AutoIncrement;
+
+struct ColumnLength {
+    int maxLength;
+}
+
+
+string getColumnDefinition(T, string member)() {
+    // Search for @Column annotation.
+    foreach(annotation; __traits(getAttributes,
+            __traits(getMember, T, member))) {
+        // Check if @Column is present.
+        static if(is(annotation == Column)) {
+            return member;
+        }
+        // Check if @Column("name") is present.
+        static if(is(typeof(annotation) == Column)) {
+            return annotation.name;
+        }
+    }
+
+    // Not found.
+    return null;
+}
+
+mixin template ActiveRecord(T : Record) {
+
+    static this() {
+        // Search through class members.
+        foreach(member; __traits(derivedMembers, T)) {
+            static if(__traits(compiles, __traits(getMember, T, member))) {
+                alias current = Target!(__traits(getMember, T, member));
+
+                // Check if this is a column.
+                static if(!(is(typeof(current) == function))) {
+                    // Find a column name.
+                    string name = getColumnDefinition!(T, member);
+
+                    // Check if the definition exists.
+                    if(name !is null) {
+                        auto info = new ColumnInfo();
+                        info.field = member;
+                        info.name = name;
+
+                        // Store the column definition.
+                        _addColumnInfo(info);
+                    }
+                }
+            }
+        }
+    }
 
     /**
      * Gets an object by its primary key.
      **/
-    static string get(KT)(KT key) {
+    static T get(KT)(KT key) {
         return null;
     }
 
     /**
      * Finds matching objects, by column values.
      **/
-    static string[] find(KT)(KT[string] key...) {
+    static T[] find(KT)(KT[string] key...) {
         return null;
     }
 
